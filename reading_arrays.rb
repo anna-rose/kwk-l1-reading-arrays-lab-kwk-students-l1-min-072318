2fdf5cdd664498681e23c019c1ca75288dfca1d5
anna-rose <917566@isd624.org>
2018-07-25 15:08:44 +0000

    automatically backed up by learn

--- reading_arrays.rb
+++ reading_arrays.rb
@@ -1,32 +1,27 @@
-STUDENT_NAMES = [
-  "Adele",
-  "Beyoncé",
-  "Cardi B",
-  "Lady Gaga",
-  "Nicki Minaj",
-  "Rihanna"
-]
+STUDENT_NAMES = ["Adele", "Beyoncé","Cardi B","Lady Gaga", "Nicki Minaj", "Rihanna"]
+
+
 
 def first_student_by_index
-  # Write a solution that returns the first student in the array student_names
+  first_student_by_index[0]
 end
 
 def fourth_student_by_index
-  # Write a solution that returns the first student in the array student_names
+  fourth_student_by_index[3]
 end
 
 def last_student_by_index
-  # Write a solution that returns the first student in the array student_names
+  last_student_by_index[-1]
 end
 
 def first_student_by_method
-  # Write a solution that returns the first student using the built in .first method
+  first_student_by_method.first 
 end
 
 def last_student_by_method
-  # Write a solution that returns the first student using the built in .last method
+  last_student_by_method.last
 end
 
 def first_second_and_third_students
-  # Write a solution that returns the first, second and third students
+  first_second_and_third_students[0,1,2]
 end
